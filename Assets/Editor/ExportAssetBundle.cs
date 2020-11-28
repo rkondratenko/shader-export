326656a6292e46eff55236ffb18784738d77e064
Rostislav Kondratenko <r.kondratenko@wwpass.com>
2020-11-28 20:34:07 +0300

    Updated the project for 2019.2.2f1 and rewritten ExportAssetBundle for that version of Unity

--- Assets/Editor/ExportAssetBundle.cs
+++ Assets/Editor/ExportAssetBundle.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using System.Linq;
 using System.IO;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class CreateAssetBundles
 {
@@ -11,17 +13,34 @@ public class CreateAssetBundles
 	{
 		// Bring up save panel
 		string path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "unity3d");
-		
-		var opts = BuildAssetBundleOptions.DeterministicAssetBundle | BuildAssetBundleOptions.ForceRebuildAssetBundle | BuildAssetBundleOptions.CollectDependencies;
-		BuildTarget[] platforms = { BuildTarget.StandaloneWindows, BuildTarget.StandaloneOSXUniversal, BuildTarget.StandaloneLinux };
-		string[] platformExts = { "-windows", "-macosx", "-linux" };
+		string directory = Path.GetDirectoryName(path);
+		string bundleName = Path.GetFileNameWithoutExtension(path);
+		string extension = Path.GetExtension(path);
 
-		for (var i = 0; i < platforms.Length; ++i)
+		var opts = BuildAssetBundleOptions.DeterministicAssetBundle | BuildAssetBundleOptions.ForceRebuildAssetBundle;
+
+		KeyValuePair<BuildTarget, string>[] platforms = {
+			new KeyValuePair<BuildTarget, string>(BuildTarget.StandaloneWindows, "-windows"),
+			new KeyValuePair<BuildTarget, string>(BuildTarget.StandaloneOSX, "-macosx"),
+			new KeyValuePair<BuildTarget, string>(BuildTarget.StandaloneLinux64, "-linux")
+		};
+		Object[] selection = Selection.GetFiltered(typeof(Shader), SelectionMode.Assets);
+		AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+		buildMap[0].assetNames = selection.Select(x => AssetDatabase.GetAssetPath(x)).ToArray();
+		// Stripping platform names from the file name
+		foreach (var platform in platforms)
 		{
-			// Build the resource file from the active selection.
-            Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
-            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path.Replace(".unity3d", platformExts[i] + ".unity3d"), opts, platforms[i]);
-			Selection.objects = selection;
+			bundleName = Regex.Replace(bundleName,$"{platform.Value}$","");
+		}
+		Debug.Log($"Bundle: {bundleName}");
+		foreach (var item in buildMap[0].assetNames)
+		{
+			Debug.Log($"Item: {item}");
+		}
+		foreach (var platform in platforms)
+		{
+			buildMap[0].assetBundleName = $"{bundleName}{platform.Value}{extension}";
+			BuildPipeline.BuildAssetBundles($"{directory}", buildMap, opts, platform.Key);
 		}
 	}
 }
